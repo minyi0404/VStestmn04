e5fe7424ec367e47c705aac15bf85f5b1eeedfdb
minyi <minyi0404@hotmail.com>
2021-08-20 21:35:54 +0800

    github上傳測試2021/08/20

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace WinFormsApp2
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // button1
@@ -40,12 +41,23 @@ namespace WinFormsApp2
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(120, 85);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(327, 242);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +68,7 @@ namespace WinFormsApp2
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
 
 namespace WinFormsApp2
 {
@@ -17,5 +19,17 @@ namespace WinFormsApp2
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                listView1.Items.Add(i + ".go");
+                listView1.Items[listView1.Items.Count - 1].BackColor = Color.Gray;
+                listView1.Items.Add("to");
+                listView1.Items[listView1.Items.Count - 1].BackColor = Color.RoyalBlue;
+                listView1.Items.Add("github");
+                listView1.Items[listView1.Items.Count - 1].BackColor = Color.Wheat;
+            }
+        }
     }
 }
